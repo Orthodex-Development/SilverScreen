42d42dfd6b293cb16e0a09f77790214fa9f1373b
Ishaan Malhi <ishaan.malhi@gmail.com>
2017-02-14 17:44:57 +0530

    feat: Add botmetrics code

--- app.rb
+++ app.rb
@@ -13,34 +13,40 @@ get '/webhook' do
 end
 
 post '/webhook' do
-    puts "PARAMS:#{params.inspect}"
-  begin
-    request.body.rewind
-    body = JSON.parse(request.body.read)
+    payload = params[:webhook]
+    entry = params[:webhook][:entry][0][:messaging][0]
 
-    entries = body[:entry]
-    puts "Entries: #{entries}"
+    if entry.has_key?(:message)
+      message = entry[:message]
+      if message[:is_echo]
+        # This is a message_echoes callback: https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-echo
+        Rails.logger.warn "---> This is a message_echoes callback (when the bot sends a reply back)"
+      else
+        Rails.logger.warn "---> This is a message callback (when the bot receives a message)"
+        # This is a message_received callback: https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
+        recipient = entry[:sender][:id]
+        text = message[:text]
 
-    entries.each do |entry|
-      entry['messaging'].each do |message|
-        text   = message[:message][:text].to_s
-        sender = message[:sender][:id]
-
-        if text.match(/hello/i)
-          greetings(sender, 'Hello from the other side!')
-        end
-
-        if text.match(/bye/i)
-          reply(sender, 'See you later dude')
-        end
-
+        resp = Facebook.new(Settings.page_access_token).message(recipient, "You said '#{text}'. Unfortunately I can't do any thing with that request")
       end
+    # This is a message_received callback: https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
+    elsif entry.has_key?(:postback)
+      Rails.logger.warn "---> This is a messaging_postbacks callback"
+    # This is a message_optins callback: https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
+    elsif entry.has_key?(:optin)
+      Rails.logger.warn "---> This is a messaging_optins callback"
+    # This is an account_linking callback: https://developers.facebook.com/docs/messenger-platform/webhook-reference/account-linking
+    elsif entry.has_key?(:account_linking)
+      Rails.logger.warn "---> This is a account_linking callback"
+    # This is a message_delivery callback: https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
+    elsif entry.has_key?(:delivery)
+      Rails.logger.warn "---> This is a message_deliveries callback"
+      # This is a message_read callback: https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-read
+    elsif entry.has_key?(:read)
+      Rails.logger.warn "---> This is a message_read callback"
     end
-  rescue Exception => e
-    puts e.message
-  end
 
-  status 200
+    render text: 'received', status: :ok
 end
 
 def reply(sender, text)
