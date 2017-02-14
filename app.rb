59a77bdbd97a5b1b14deaf1a47da5eec7ed80704
Ishaan Malhi <ishaan.malhi@gmail.com>
2017-02-14 17:11:30 +0530

    fix: json read fix

--- app.rb
+++ app.rb
@@ -1,46 +1,50 @@
 require 'json'
 require 'sinatra'
+require "sinatra/namespace"
 require 'httparty'
 
 set :endpoint, "https://graph.facebook.com/v2.6/me/messages?access_token=#{ENV['PAGE_TOKEN']}"
-set :port, ENV['PORT'] || 4002
 
-get '/' do
-  if params['hub.verify_token'] == ENV['VERIFY_TOKEN']
-    render text: params['hub.challenge'], status: :ok
-  else
-    render text: 'invalid token', status: :forbidden
-  end
-end
-
-post '/' do
-  begin
-    request.body.rewind
-    body = JSON.parse(request.body.read)
-
-    entries = body[:entry]
-    puts "Entries: #{entries}"
-
-    entries.each do |entry|
-      entry['messaging'].each do |message|
-        text   = message[:message][:text].to_s
-        sender = message[:sender][:id]
-
-        if text.match(/hello/i)
-          greetings(sender, 'Hello from the other side!')
-        end
-
-        if text.match(/bye/i)
-          reply(sender, 'See you later dude')
-        end
-
-      end
+namespace '/webhook' do
+  get '/' do
+    if params['hub.verify_token'] == ENV['VERIFY_TOKEN']
+      render text: params['hub.challenge'], status: :ok
+    else
+      render text: 'invalid token', status: :forbidden
     end
-  rescue Exception => e
-    puts e.message
   end
 
-  status 200
+  post '/' do
+      puts request.body
+    begin
+      request.body.rewind
+      body = JSON.parse(request.body.read)
+
+      entries = body[:entry]
+      puts "Entries: #{entries}"
+
+      entries.each do |entry|
+        entry['messaging'].each do |message|
+          text   = message[:message][:text].to_s
+          sender = message[:sender][:id]
+
+          if text.match(/hello/i)
+            greetings(sender, 'Hello from the other side!')
+          end
+
+          if text.match(/bye/i)
+            reply(sender, 'See you later dude')
+          end
+
+        end
+      end
+    rescue Exception => e
+      puts e.message
+    end
+
+    status 200
+  end
+
 end
 
 def reply(sender, text)
