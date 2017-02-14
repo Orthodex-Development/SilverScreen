9aee9794cd3c4e05ba3453e0804581c586c8dca2
Ishaan Malhi <ishaan.malhi@gmail.com>
2017-02-14 17:29:39 +0530

    fix: webhooks sub fix

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source 'https://rubygems.org'
 
 gem 'json'      , '~> 1.8.3'
 gem 'sinatra'   , '~> 1.4.7'
-gem 'sinatra-namespace'
 gem 'httparty'  , '~> 0.13.7'
--- app.rb
+++ app.rb
@@ -1,50 +1,46 @@
 require 'json'
 require 'sinatra'
-require "sinatra/namespace"
 require 'httparty'
 
 set :endpoint, "https://graph.facebook.com/v2.6/me/messages?access_token=#{ENV['PAGE_TOKEN']}"
 
-namespace '/webhook' do
-  get '/' do
-    if params['hub.verify_token'] == ENV['VERIFY_TOKEN']
-      render text: params['hub.challenge'], status: :ok
-    else
-      render text: 'invalid token', status: :forbidden
-    end
+get '/webhook' do
+  if params['hub.verify_token'] == ENV['VERIFY_TOKEN']
+    params['hub.challenge']
+  else
+    render text: 'invalid token', status: :forbidden
   end
+end
 
-  post '/' do
-      puts request.body
-    begin
-      request.body.rewind
-      body = JSON.parse(request.body.read)
+post '/' do
+    puts request.body
+  begin
+    request.body.rewind
+    body = JSON.parse(request.body.read)
 
-      entries = body[:entry]
-      puts "Entries: #{entries}"
+    entries = body[:entry]
+    puts "Entries: #{entries}"
 
-      entries.each do |entry|
-        entry['messaging'].each do |message|
-          text   = message[:message][:text].to_s
-          sender = message[:sender][:id]
+    entries.each do |entry|
+      entry['messaging'].each do |message|
+        text   = message[:message][:text].to_s
+        sender = message[:sender][:id]
 
-          if text.match(/hello/i)
-            greetings(sender, 'Hello from the other side!')
-          end
-
-          if text.match(/bye/i)
-            reply(sender, 'See you later dude')
-          end
-
+        if text.match(/hello/i)
+          greetings(sender, 'Hello from the other side!')
         end
-      end
-    rescue Exception => e
-      puts e.message
-    end
 
-    status 200
+        if text.match(/bye/i)
+          reply(sender, 'See you later dude')
+        end
+
+      end
+    end
+  rescue Exception => e
+    puts e.message
   end
 
+  status 200
 end
 
 def reply(sender, text)
